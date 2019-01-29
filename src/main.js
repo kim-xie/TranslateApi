54d1fe4f83347a578bf2885e1ba03bf08fd0303f
kim <1134771121@qq.com>
2019-01-29 20:22:01 +0800

    语音翻译

--- src/main.js
+++ src/main.js
@@ -12,9 +12,28 @@ const https = require('https')
 const path = require('path')
 const querystring = require("querystring")
 const Translator = require('./index.js')
+const multer = require('multer')
+//上传文件配置
+const storage = multer.diskStorage({
+  //文件存储位置
+  destination: (req, file, cb) => {
+    cb(null, path.resolve(__dirname, 'src/temp/'));
+  },
+  //文件名
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}_${Math.ceil(Math.random() * 1000)}_multer.${file.originalname.split('.').pop()}`);
+  }
+});
+const uploadCfg = {
+  storage: storage,
+  limits: {
+    //上传文件的大小限制,单位bytes
+    fileSize: 1024 * 1024 * 20
+  }
+};
 
-app.get("/translation", function (req, res, next) {
-  console.log('although this matches');
+// 文本翻译
+app.post("/text_translation", function (req, res, next) {
   // 设置接收数据编码格式为 UTF-8
   req.setEncoding('utf-8')
   let postData = ""
@@ -24,7 +43,6 @@ app.get("/translation", function (req, res, next) {
   })
   // 数据接收完毕，执行回调函数
   req.addListener("end", () => {
-      console.log('数据接收完毕')
 			let objectData = JSON.parse(postData)
 			const from = objectData.from
 			const to = objectData.to
@@ -33,7 +51,6 @@ app.get("/translation", function (req, res, next) {
 			textTranslate(from, to, text, 'src/temp/tts.mp3')
 			.then(getAudio)
 			.then(result => {
-				//console.log(result);
 				res.setHeader("Access-Control-Allow-Origin", "*")
 				res.writeHead(200, {
             "Content-Type": "text/plain;charset=utf-8"
@@ -51,10 +68,51 @@ app.get("/translation", function (req, res, next) {
   })
 })
 
-// 创建http服务
-// http.createServer(app).listen('8888', "localhost", function(){
-//     console.log("HTTP服务器启动中，监听端口：8888......")
-// })
+// 语音翻译
+app.post("/voice_translation", function (req, res, next) {
+  // 设置接收数据编码格式为 UTF-8
+  req.setEncoding('utf-8')
+  let postData = ""
+  // 数据块接收中
+  req.addListener("data", postDataChunk => {
+      postData += postDataChunk
+  })
+  // 数据接收完毕，执行回调函数
+  req.addListener("end", () => {
+			let objectData = JSON.parse(postData)
+			const from = objectData.from
+			const to = objectData.to
+			const fileName = objectData.fileName
+			audioTranslate(fileName, from, to).then(getAudio).then(result => {
+				res.setHeader("Access-Control-Allow-Origin", "*")
+				res.writeHead(200, {
+            "Content-Type": "text/plain;charset=utf-8"
+        })
+				res.write(JSON.stringify(result))
+				res.end()
+			}).catch(error => {
+				console.log(error)
+				res.writeHead(500, {
+            "Content-Type": "text/plain;charset=utf-8"
+        })
+				res.end()
+			})
+  })
+})
+
+// 语音上传
+app.post("/upload_voice", async (req, res) => {
+  let upload = multer(uploadCfg).any()
+  upload(req, res, async (err) => {
+    if (err) {
+      console.log(err);
+      return;
+    };
+    console.log(req.files);
+    let uploadFile = req.files[0];
+    res.json({ path: `src/temp/${uploadFile.filename}` });
+  });
+})
 
 var server = app.listen(8888, function () {
   var host = server.address().address
@@ -173,13 +231,18 @@ function audioTranslate(fileName, from, to){
 				Translator.translate(from, to, text).then((success) => {
 					console.log("translate: "+ JSON.stringify(success))
 					const dst = success.trans_result[0].dst
+          const src = success.trans_result[0].src
 					if(to === 'yue'){
 						to = 'cte'
 					}
-					Translator.text2audio(to, dst, fileName).then((success) => {
-						console.log("text2audio: success")
-						resolve()
-					})
+          let result = {
+            src,
+    				dst,
+    				to,
+    				fileName: path.resolve(fileName),
+    				data: JSON.stringify(success)
+    			}
+    			resolve(result)
 				})
 			})
 		}).catch((error) => {
